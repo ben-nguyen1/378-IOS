80e377cecac72a8e14d46223a2e91b227286f2dc
Ben Nguyen <pride@laptop.local>
2017-10-20 10:00:28 -0500

    Add keyboard dismiss to login and create account screens

--- BuildABudget/BuildABudget/CreateAccountViewController.swift
+++ BuildABudget/BuildABudget/CreateAccountViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CreateAccountViewController: UIViewController {
+class CreateAccountViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var create: UIButton!
     
@@ -29,15 +29,19 @@ class CreateAccountViewController: UIViewController {
         // add placeholder text for all text fields
         firstName.text = nil
         firstName.placeholder = "First Name"
+        firstName.delegate = self
         
         lastName.text = nil
         lastName.placeholder = "Last Name"
+        lastName.delegate = self
         
         email.text = nil
         email.placeholder = "Email Address"
+        email.delegate = self
         
         pass.text = nil
         pass.placeholder = "Password"
+        pass.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -98,5 +102,26 @@ class CreateAccountViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // This method is called when the user touches the Return key on the
+    // keyboard. The 'textField' passed in is a pointer to the textField
+    // the cursor was in at the time they touched the Return key on the
+    // keyboard.
+    //
+    // From the Apple documentation: Asks the delegate if the text field
+    // should process the pressing of the return button.
+    //
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 'First Responder' is the same as 'input focus'.
+        // We are removing input focus from the text field.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Called when the user touches on the main view (outside the UITextField).
+    //
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
--- BuildABudget/BuildABudget/LoginViewController.swift
+++ BuildABudget/BuildABudget/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var login: UIButton!
     @IBOutlet weak var register: UIButton!
@@ -21,15 +21,16 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         login.layer.cornerRadius = 5
         register.layer.cornerRadius = 5
         
         email.text = nil
         email.placeholder = "Email Address"
+        email.delegate = self;
         
         pass.text = nil
         pass.placeholder = "Password"
+        pass.delegate = self;
     }
     
     override func didReceiveMemoryWarning() {
@@ -63,6 +64,27 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func goCreateBtn(_ sender: Any) {
+    }
+    
+    // This method is called when the user touches the Return key on the
+    // keyboard. The 'textField' passed in is a pointer to the textField
+    // the cursor was in at the time they touched the Return key on the
+    // keyboard.
+    //
+    // From the Apple documentation: Asks the delegate if the text field
+    // should process the pressing of the return button.
+    //
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 'First Responder' is the same as 'input focus'.
+        // We are removing input focus from the text field.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Called when the user touches on the main view (outside the UITextField).
+    //
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
 
