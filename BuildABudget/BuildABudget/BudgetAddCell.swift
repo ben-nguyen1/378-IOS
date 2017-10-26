cf2701f3c5e34d81f84aa9f075d540ec56957d27
chris <chris-c2017@outlook.com>
2017-10-26 17:41:27 -0500

    added inViewTable attribute, ShowAlertProtocol object, and updated addButton action code

--- BuildABudget/BuildABudget/BudgetAddCell.swift
+++ BuildABudget/BuildABudget/BudgetAddCell.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class BudgetAddCell: UITableViewCell {
     
+    var inViewTable: Int = 0
+    let BudgetAddCellAccess = AccessService.access
     
+    //UIAlert field vars
+    //var descriptionTextField: UITextField? = nil
+    //var dueDateTextField: UITextField? = nil
+    //var totalDueTextField: UITextField? = nil //Double? = nil
+    
+    //Delegate that enables us to launch AlertWindows
+    var delegate: ShowAlertProtocol?
+    
+    @IBAction func addButton(_ sender: Any) {
 
-    @IBOutlet weak var addButton: UIButton!
-    
-    
+        self.delegate?.launchAlertWindow(tableValueInput: inViewTable)
+    }
+        
+
+
+/*
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -21,6 +35,6 @@ class BudgetAddCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
+ */
 }
 
