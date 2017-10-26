fcbb017008be88412a0543d8cba11fbc08a6c39a
chris <superchris@dhcp-10-149-11-53.ae.utexas.edu>
2017-10-26 13:35:22 -0500

    Updated BudgetAddCell

---
+++ BuildABudget/BuildABudget/BudgetAddCell.swift
@@ -0,0 +1,26 @@
+//
+//  AddCell.swift
+//  BuildABudget
+//
+//  Created by chris on 10/24/17.
+//  Copyright © 2017 Chris Cale. All rights reserved.
+//
+import UIKit
+
+class BudgetAddCell: UITableViewCell {
+    
+    
+
+    @IBOutlet weak var addButton: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
+
