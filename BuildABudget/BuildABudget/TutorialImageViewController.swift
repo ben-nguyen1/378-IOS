a7980c7e04d7155213bbc8274eeeb59c550f51b3
Ben Nguyen <pride@wireless-10-147-233-140.public.utexas.edu>
2017-11-16 17:06:30 -0600

    Update tutorial screen

--- BuildABudget/BuildABudget/TutorialImageViewController.swift
+++ BuildABudget/BuildABudget/TutorialImageViewController.swift
@@ -35,7 +35,7 @@ class TutorialImageViewController: UIViewController {
         } else if (fileName == "4") {
             label.text = "The checklist screen lets you track what expenses you still need to pay off. Monthly expenses from the budget are also shown here. You can also add more expenses."
         } else if (fileName == "5") {
-            label.text = "In the goals screen, you can add financial goals that you want to work towards. Examples of goals can be a vacation or a new car. A progress bar is shown under each goal as well as a percentage to indicate completion"
+            label.text = "In the goals screen, you can add financial goals that you want to work towards. A progress bar is shown under each goal as well as a percentage to indicate completion. You can click on an existing goal to edit it."
         } else if (fileName == "6") {
             label.text = "Here, you can config an existing goal or fill out a new goal."
         }
